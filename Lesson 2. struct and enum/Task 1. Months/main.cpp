c530d8894f5f3536ffcd025e517b14304bf61aca
Sergey Ovsyannikov <ovssv87@gmail.com>
2023-12-19 23:12:45 +0400

    Changed Task 1 and Task 2 in Lesson 2

--- Lesson 2. struct and enum/Task 1. Months/main.cpp
+++ Lesson 2. struct and enum/Task 1. Months/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-#include <string>
+
 
 // перечисление Месяцы
 enum class Months
@@ -18,47 +18,51 @@ enum class Months
     December
 };
 
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
     int inValueUser{ 1 };
+    
     while (inValueUser)
     {
         std::cout << "Введите номер месяца или введите '0' для выхода: ";
         std::cin >> inValueUser;
 
+        auto castValueUser = static_cast<Months>(inValueUser);
+
         if (inValueUser == 0)
         {
             return 0;
         }
-        switch (inValueUser)
+        switch (castValueUser)
         {
-        case static_cast<int>(Months::January):std::cout << "Январь" << std::endl;
+        case Months::January : std::cout << "Январь" << std::endl;
             break;
-        case static_cast<int>(Months::February):std::cout << "Февраль" << std::endl;
+        case Months::February : std::cout << "Февраль" << std::endl;
             break;
-        case static_cast<int>(Months::March):std::cout << "Март" << std::endl;
+        case Months::March : std::cout << "Март" << std::endl;
             break;
-        case static_cast<int>(Months::April):std::cout << "Апрель" << std::endl;
+        case Months::April : std::cout << "Апрель" << std::endl;
             break;
-        case static_cast<int>(Months::May):std::cout << "Май" << std::endl;
+        case Months::May : std::cout << "Май" << std::endl;
             break;
-        case static_cast<int>(Months::June):std::cout << "Июнь" << std::endl;
+        case Months::June : std::cout << "Июнь" << std::endl;
             break;
-        case static_cast<int>(Months::July):std::cout << "Июль" << std::endl;
+        case Months::July : std::cout << "Июль" << std::endl;
             break;
-        case static_cast<int>(Months::August):std::cout << "Август" << std::endl;
+        case Months::August : std::cout << "Август" << std::endl;
             break;
-        case static_cast<int>(Months::September):std::cout << "Сентябрь" << std::endl;
+        case Months::September : std::cout << "Сентябрь" << std::endl;
             break;
-        case static_cast<int>(Months::October):std::cout << "Октябрь" << std::endl;
+        case Months::October : std::cout << "Октябрь" << std::endl;
             break;
-        case static_cast<int>(Months::November):std::cout << "Ноябрь" << std::endl;
+        case Months::November : std::cout << "Ноябрь" << std::endl;
             break;
-        case static_cast<int>(Months::December):std::cout << "Декабрь" << std::endl;
+        case Months::December : std::cout << "Декабрь" << std::endl;
             break;
-        default:std::cout << "Введено неправильное число! Введите число от 1 до 12!\n" << std::endl;
+        default : std::cout << "Введено неправильное число! Введите число от 1 до 12!\n" << std::endl;
             break;
         }
     }
