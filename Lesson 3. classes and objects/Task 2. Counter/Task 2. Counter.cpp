a923852362d728bf23c9eb4935bd661f59ef71e8
Sergey Ovsyannikov <ovssv87@gmail.com>
2023-12-23 16:45:29 +0400

    Update Task 2 in Lesson 3

--- Lesson 3. classes and objects/Task 2. Counter/Task 2. Counter.cpp
+++ Lesson 3. classes and objects/Task 2. Counter/Task 2. Counter.cpp
@@ -1,7 +1,75 @@
 ﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class Counter
+{
+public:
+    Counter(int value)
+    {
+        set_value(value);
+    }
+    Counter()
+    {
+        value = 1;
+    }
+
+    void increment() { this->value = value++; } 
+    void dicrement() { this->value = value--; }
+    int get_value() { return value; }
+    void set_value(int value) { this->value = value; }
+
+private:
+    int value;
+};
+void comand(char sym, Counter& count)
+{
+    while (sym != 'x')
+    {
+        std::cout << "Введите команду('+', '-', '=' или 'x'): ";
+        std::cin >> sym;
+        if (sym == '+')
+        {
+            count.increment();
+        }
+        else if (sym == '-')
+        {
+            count.dicrement();
+        }
+        else if (sym == '=')
+        {
+            std::cout << count.get_value() << std::endl;
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string yesOrNot = "нет";
+    int value{ 0 };
+    char sym = '=';
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите 'да' или 'нет': ";
+    std::cin >> yesOrNot;
+    std::cout << std::endl;
+
+    if (yesOrNot == "да")
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> value;
+        Counter* count = new Counter(value);
+        comand(sym, *count);
+        delete count;
+    }
+    else
+    {
+        Counter* count = new Counter();
+        comand(sym, *count);
+        delete count;
+    } 
+    return 0;
 }
 
