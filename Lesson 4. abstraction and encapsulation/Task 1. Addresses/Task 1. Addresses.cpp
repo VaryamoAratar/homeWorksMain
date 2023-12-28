c0b6600f78f95dd0fa16b8070752c4a32475ff91
Sergey Ovsyannikov <ovssv87@gmail.com>
2023-12-28 23:08:04 +0400

    Update Task 1 and Task 2 in Lesson 4

--- Lesson 4. abstraction and encapsulation/Task 1. Addresses/Task 1. Addresses.cpp
+++ Lesson 4. abstraction and encapsulation/Task 1. Addresses/Task 1. Addresses.cpp
@@ -2,39 +2,55 @@
 #include <string>
 #include <fstream>
 
-//класс Адреса
-class Addresses
+//класс Адрес
+class Address
 {
 public:
-    Addresses() //можно инициализировать со значениями по умолчанию, но не знаю есть ли смысл?
+    Address(std::string nameCity, std::string nameStreet, int numHouse, int numApart) :
+        nameCity{ nameCity }, nameStreet{ nameStreet }, numHouse{ numHouse }, numApart{ numApart }
     {}
+
     //сеттеры
     void set_nameCity(std::string nameCity) { this->nameCity = nameCity; }
     void set_nameStreet(std::string nameStreet) { this->nameStreet = nameStreet; }
     void set_numHouse(int numHouse) { this->numHouse = numHouse; }
     void set_numApart(int numApart) { this->numApart = numApart; }
+
     //геттеры
     int get_numHouse() { return numHouse; }
     int get_numApart() { return numApart; }
     void get_nameCity(std::string& outName) { outName = nameCity; }
     void get_nameStreet(std::string& outName) { outName = nameStreet; }
 
+    //создаем строку для вывода
+    std::string get_output_address(std::string& outAddress)
+    {
+        outAddress = nameCity + ',' + ' ' + nameStreet + ',' + ' ' + std::to_string(numHouse) + ',' + ' ' + std::to_string(numApart);
+        return outAddress;
+    }
 
 private:
     std::string nameCity;   //Название города
     std::string nameStreet; //Название улицы
     int numHouse;           //Номер дома
     int numApart;           //Номер квартиры
-
 };
+
+void sort(Address** address, int size)
+{
+}
 
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    std::string word;
+    std::string stringIn;
     int nSize{ 0 };
+    std::string nameCity;
+    std::string nameStreet;
+    int numHouse;
+    int numApart;
 
     //открываем in.txt
     std::ifstream fIn("in.txt");
@@ -47,28 +63,23 @@ int main()
     }
 
     //считываем сколько адресов в файле
-    fIn >> word;
-    nSize = std::stoi(word);
+    fIn >> stringIn;
+    nSize = std::stoi(stringIn);
 
-    //создаем массив объектов
-    Addresses* address = new Addresses[nSize];
+    //создаем массив указателей объектов
+    Address** address = new Address * [nSize];
 
-    //считываем из файла и заполняем поля обектов
+    //считываем из файла и создаем обекты с инициализацией
     for (int i = 0; i < nSize; i++)
     {
-        int numHouse;
-        int numApart;
+        fIn >> nameCity;
+        fIn >> nameStreet;
+        fIn >> stringIn;
+        numHouse = std::stoi(stringIn);
+        fIn >> stringIn;
+        numApart = std::stoi(stringIn);
 
-        fIn >> word;
-        address[i].set_nameCity(word);
-        fIn >> word;
-        address[i].set_nameStreet(word);
-        fIn >> word;
-        numHouse = std::stoi(word);
-        address[i].set_numHouse(numHouse);
-        fIn >> word;
-        numApart = std::stoi(word);
-        address[i].set_numApart(numApart);
+        address[i] = new Address(nameCity, nameStreet, numHouse, numApart);
     }
 
     //закрываем in.txt
@@ -81,18 +92,15 @@ int main()
     fOut << nSize << std::endl;
     for (int i = nSize - 1; i >= 0; i--)
     {
-        address[i].get_nameCity(word);
-        fOut << word << ',' << ' ';
-        address[i].get_nameStreet(word);
-        fOut << word << ',' << ' ';
-        fOut << address[i].get_numHouse() << ',' << ' ';
-        fOut << address[i].get_numApart() << std::endl;
+        address[i]->get_output_address(stringIn);
+        fOut << stringIn << std::endl;
+        delete address[i];
     }
 
     //закрываем out.txt
     fOut.close();
 
-    //удаляем массив объектов
+    //удаляем массив указателей объектов
     delete[] address;
 
     return 0;
--- Lesson 4. abstraction and encapsulation/Task 2. sorting addresses/Task 2. sorting addresses.cpp
+++ Lesson 4. abstraction and encapsulation/Task 2. sorting addresses/Task 2. sorting addresses.cpp
@@ -1,20 +1,127 @@
-﻿// Task 2. sorting addresses.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <fstream>
 
-#include <iostream>
+//класс Адрес
+class Address
+{
+public:
+    Address(std::string nameCity, std::string nameStreet, int numHouse, int numApart) :
+        nameCity{ nameCity }, nameStreet{ nameStreet }, numHouse{ numHouse }, numApart{ numApart }
+    {}
+
+    //сеттеры
+    void set_nameCity(std::string nameCity) { this->nameCity = nameCity; }
+    void set_nameStreet(std::string nameStreet) { this->nameStreet = nameStreet; }
+    void set_numHouse(int numHouse) { this->numHouse = numHouse; }
+    void set_numApart(int numApart) { this->numApart = numApart; }
+
+    //геттеры
+    int get_numHouse() { return numHouse; }
+    int get_numApart() { return numApart; }
+    void get_nameCity(std::string& outName) { outName = nameCity; }
+    void get_nameStreet(std::string& outName) { outName = nameStreet; }
+
+    //создаем строку для вывода
+    std::string get_output_address(std::string& outAddress)
+    {
+        outAddress = nameCity + ',' + ' ' + nameStreet + ',' + ' ' + std::to_string(numHouse) + ',' + ' ' + std::to_string(numApart);
+        return outAddress;
+    }
+    
+private:
+    std::string nameCity;   //Название города
+    std::string nameStreet; //Название улицы
+    int numHouse;           //Номер дома
+    int numApart;           //Номер квартиры
+};
+
+//сортировка пузырьком по названию города
+void sort(Address** address, int nSize)
+{
+    for (int i = 0; i < nSize; i++) {
+        for (int j = 0; j < nSize - 1 - i; j++) {
+            std::string name1;
+            address[j]->get_nameCity(name1);
+            std::string name2;
+            address[j + 1]->get_nameCity(name2);
+            if (name1 > name2)
+            {
+                *address[nSize] = *address[j];
+                *address[j] = *address[j + 1];
+                *address[j + 1] = *address[nSize];
+            }
+        }
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    std::string stringIn;
+    int nSize{ 0 };
+    std::string nameCity;
+    std::string nameStreet;
+    int numHouse;
+    int numApart;
+
+    //открываем in.txt
+    std::ifstream fIn("in.txt");
+
+    //проверяем, открылся ли файл.
+    if (!fIn.is_open())
+    {
+        std::cout << "File opening error!";
+        return 0;
+    }
+
+    //считываем сколько адресов в файле
+    fIn >> stringIn;
+    nSize = std::stoi(stringIn);
+
+    //создаем массив указателей объектов
+    Address** address = new Address * [nSize + 1];
+
+    //считываем из файла и создаем обекты с инициализацией
+    for (int i = 0; i < nSize; i++)
+    {
+        fIn >> nameCity;
+        fIn >> nameStreet;
+        fIn >> stringIn;
+        numHouse = std::stoi(stringIn);
+        fIn >> stringIn;
+        numApart = std::stoi(stringIn);
+
+        address[i] = new Address(nameCity, nameStreet, numHouse, numApart);
+    }
+
+    //закрываем in.txt
+    fIn.close();
+
+    address[nSize] = new Address(nameCity, nameStreet, numHouse, numApart);
+    sort(address, nSize);
+    delete address[nSize];
+
+    //открываем для записи out.txt
+    std::ofstream fOut("out.txt");
+
+    //запись данных в файл out.txt
+    fOut << nSize << std::endl;
+    for (int i =  0; i <nSize; i++)
+    {
+        address[i]->get_output_address(stringIn);
+        fOut << stringIn << std::endl;
+        delete address[i];
+    }
+
+    //закрываем out.txt
+    fOut.close();
+    
+    //удаляем массив указателей объектов
+    delete[] address;
+
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
