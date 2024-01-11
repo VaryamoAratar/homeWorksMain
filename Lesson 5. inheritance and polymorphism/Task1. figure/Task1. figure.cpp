96df1ce8c915508f234ecb3936659f7f8ee61c1e
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-01-11 22:08:53 +0400

    Update Task1 in Lesson5

--- Lesson 5. inheritance and polymorphism/Task1. figure/Task1. figure.cpp
+++ Lesson 5. inheritance and polymorphism/Task1. figure/Task1. figure.cpp
@@ -6,18 +6,17 @@ class Figure
 public:
     Figure()
     {
-        std::cout << "Количество сторон:" << std::endl;
         std::cout << get_name() << ':' << ' ' << get_sides_count() << std::endl;
     }
-    int get_sides_count() { return sides_count; }
-    std::string get_name() { return (std::string(name)); }
 
 protected:
     Figure(int sides_count, std::string name) : sides_count{ sides_count }, name { name }
     {
         std::cout << get_name() << ':' << ' ' << get_sides_count() << std::endl;
     }
-    
+    int get_sides_count() { return sides_count; }
+    std::string get_name() { return (std::string(name)); }
+
 private:
     int sides_count = 0;
     std::string name = "Фигура";
@@ -34,13 +33,13 @@ class Quadrangle : public Figure
 {
 public:
     Quadrangle() : Figure(4, "Квадрат") {};
-
 };
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
+    std::cout << "Количество сторон:" << std::endl;
     Figure var1;
     Triangle var2;
     Quadrangle var3;
