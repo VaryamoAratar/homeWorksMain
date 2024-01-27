0890db3ba014661a86fd34170a1a4a39bf717ca1
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-01-27 16:43:51 +0400

    Updated Task1 in Lesson9

--- Lesson 9. Operator overloading/Task1. Comparisons in fractions/Task1. Comparisons in fractions.cpp
+++ Lesson 9. Operator overloading/Task1. Comparisons in fractions/Task1. Comparisons in fractions.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
 
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	bool operator == (Fraction other) { return numerator_ * other.denominator_ == denominator_ * other.numerator_; }
+	bool operator != (Fraction other) { return !(*this==other); }
+	bool operator < (Fraction other) { return numerator_ * other.denominator_ < denominator_ * other.numerator_; }
+	bool operator > (Fraction other) { return  other < *this; }
+	bool operator <= (Fraction other) { return !(*this > other); }
+	bool operator >= (Fraction other) { return !(*this < other); }
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
 }
